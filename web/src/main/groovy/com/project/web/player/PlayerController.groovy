9404506adbff1f48d175e013bb2b74c56e14063d
Sam Halligan <samhalligan15@gmail.com>
2015-03-22 15:32:43 +0100

    re

--- web/src/main/groovy/com/project/web/player/PlayerController.groovy
+++ web/src/main/groovy/com/project/web/player/PlayerController.groovy
@@ -98,14 +98,14 @@ class PlayerController {
   Create a player
    */
     @RequestMapping(method = RequestMethod.POST, value = "/{version}/teams/{teamId}/players")
-    def createPlayer(@PathVariable String version, @PathVariable Long teamId, @RequestBody(required = true) CreateMultiplePlayersRequest request) {
+    def createPlayerRequest(@PathVariable String version, @PathVariable Long teamId, @RequestBody(required = true) CreateMultiplePlayersRequest request) {
         Team team = teamService.findTeamById(teamId)
         if (team == null){
             //Do nothing for now - should throw some form of exception
         }
 
         List<Long> playerIds = new ArrayList()
-        for (CreatePlayerRequest playerRequest : request) {
+        for (CreatePlayerRequest playerRequest : request.requestList) {
             Player player = new Player(firstName: playerRequest.firstName, lastName: playerRequest.lastName, team: team, playerNumber: playerRequest.playerNumber)
 
             Long playerId = playerService.createPlayer(player)
